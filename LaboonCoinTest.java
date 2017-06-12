543037f841c2a5703f9b28760ece982b919af850
Daniel O'Neill <dgo5@pitt.edu>
2017-06-12 15:52:04 -0400

    fixed comments, added tests

--- LaboonCoinTest.java
+++ LaboonCoinTest.java
@@ -4,34 +4,33 @@ import org.junit.*;
 
 /*
 *	JUST AS A NOTE:
-*	Please try to make tests as localized as possible through the use of
-*	doubles/stubs if necessary.
-*
+*	Ideally, each test should test exactly one method.
 */
 public class LaboonCoinTest {
 
-	LaboonCoin l = null;
 	//	Creates an instance of LaboonCoin
-	//	for testing
+	//	prior to each test
+	LaboonCoin l = null;
 	@Before
 	public void init(){
 		l = new LaboonCoin();		
 	}
-    	// Assert that creating a new LaboonCoin instance
-    	// does not return a null reference
+	
+    	//	Assert that creating a new LaboonCoin instance
+    	//	does not return a null reference
     	@Test
     	public void testLaboonCoinExists() {
 		assertNotNull(l);	
 	}
     
-	//Assert that the program does not crash when
-	//asked to print an empty blockchain
+	//	Assert that the program does not crash when
+	//	asked to print an empty blockchain
 	@Test
 	public void testNullPrint(){
 		l.getBlockChain();		
 	}
 	
-	//Tests the happy path of the hash function
+	//	Tests the happy path of the hash function
 	@Test
 	public void testHash(){
 		int hash1 = l.hash("boo");
@@ -40,6 +39,9 @@ public class LaboonCoinTest {
 		assertEquals(hash2, 2034739681);
 	}
 	
+	//	Tests corner case of empty hash
+	//	to ensure initial hash value
+	//	remains unchanged
 	@Test
 	public void testEmptyHash(){
 		assertEquals(l.hash(""), 10000000);
@@ -54,6 +56,8 @@ public class LaboonCoinTest {
 		assertTrue(l.validHash(6, 212));
 	}
 	
+	//	Tests edge cases of hashes with less difficulty than
+	//	necessary.
 	@Test
 	public void testLessDifficulty(){
 		assertTrue(l.validHash(2, 1038730));
@@ -61,6 +65,8 @@ public class LaboonCoinTest {
 		assertTrue(l.validHash(5, 212));
 	}
 	
+	//	Tests edge cases of hashes with more difficulty than
+	//	necessary.
 	@Test
 	public void testMoreDifficulty(){
 		assertFalse(l.validHash(4, 1038730));
@@ -68,21 +74,16 @@ public class LaboonCoinTest {
 		assertFalse(l.validHash(7, 212));
 	}
 	
+	//	Tests for valid nonce cases based off
+	//	expected deterministic nonce values.
 	@Test
 	public void testNonce(){
 		assertEquals(l.mine("boo", 738238, 3), 1441);
 		assertEquals(l.mine("quock", 1005884, 3), 553);
 		assertEquals(l.mine("mars", 589727, 3), 1299);
 	}
-    
-	//Assert that the program does not crash when
-	//asked to print an empty blockchain
-	@Test
-	public void testNullPrint(){
-		l.getBlockChain();		
-	}
 	
-	//Tests the happy path of a nonempty blockchain
+	//	Tests the happy path of a nonempty blockchain
 	@Test
 	public void testHappyBlockPrint(){
 		l.blockchain.add("Johnny gave Ustes $50");
@@ -91,7 +92,7 @@ public class LaboonCoinTest {
 		assertEquals("Johnny gave Ustes $50" + '\n' + "Krombopulos gave Michael $1000" + '\n'  + "Jimmy gave Pitt $30000" + '\n', l.getBlockChain());
 	}
 	
-	//Tests if hash has zero 0 in front, if so invalid
+	//	Tests if hash has zero 0 in front, if so invalid
 	@Test
 	public void testInvalidZero{
 		l.createBlock("Johnny gave Ustes $50", 0343243, 0212312, 0423432);
@@ -99,7 +100,7 @@ public class LaboonCoinTest {
 		assertEquals();
 	}
 	
-	//Tests if hash has 1 0 in front, if so invalid 
+	//	Tests if hash has 1 0 in front, if so invalid 
 	@Test
 	public void testInvalidMultZeros{
 		l.createBlock("Johnny gave Ustes $50", 0043243, 0012312, 0023432);
@@ -107,7 +108,7 @@ public class LaboonCoinTest {
 		assertEquals();
 	}
 	
-	//Tests if hash has 3 or more in front, if so valid
+	//	Tests if hash has 3 or more in front, if so valid
 	@Test
 	public void testValidZeros{
 		l.createBlock("Johnny gave Ustes $50", 0000043, 0000312, 0003432);
